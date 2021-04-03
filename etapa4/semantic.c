3c4ce79dbc6ccd3ff99b5b34b2fced9536d60fec
Gabriel Rizzolli Partichelli <gabrielpartichelli@gmail.com>
2021-04-03 19:50:07 -0300

    Fix files

--- etapa4/semantic.c
+++ etapa4/semantic.c
@@ -1,9 +1,16 @@
 int semantic_errors = 0;
 
+#include "semantic.h"
 
+void check_and_set_declarations(ast* n){
+
+
+
+}
 
 
 int get_semantic_errors(){
 
 	return semantic_errors;
 }
+
--- etapa4/semantic.h
+++ etapa4/semantic.h
@@ -1,7 +1,8 @@
 #ifndef SEMANTIC_HEADER
 #define SEMANTIC_HEADER
 
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "hash.h"
 #include "ast.h"
 
